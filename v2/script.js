057d3f09323f9166721ec2c25c36ae51e9980d75
sampath-wos <sampathb@whiteorangesoftware.com>
2023-02-09 11:48:18 +0530

    todo complete

--- v2/script.js
+++ v2/script.js
@@ -1,6 +1,10 @@
 let todoInput = document.getElementById("AddToDoData"), // Input where todo is coming
   todoForm = document.getElementById("ToDoForm"), // Form where todo will submit
   todoTableBody = document.getElementById("ToDoTableBody"),
+  todoSelectAll = document.getElementById("selectAllTodo"),
+  selectedTodoDeleteBtn = document.getElementById("selectedTodoDeleteBtn"),
+  deleteAllTodo = document.getElementById("allTodoDeleteBtn"),
+  SearchTodoData = document.getElementById("SearchTodoData"),
   updateIcon = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none"><path d="m13.26 3.6-8.21 8.69c-.31.33-.61.98-.67 1.43l-.37 3.24c-.13 1.17.71 1.97 1.87 1.77l3.22-.55c.45-.08 1.08-.41 1.39-.75l8.21-8.69c1.42-1.5 2.06-3.21-.15-5.3-2.2-2.07-3.87-1.34-5.29.16Z" stroke="currentcolor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path><path d="M11.89 5.05a6.126 6.126 0 0 0 5.45 5.15M3 22h18" stroke="currentcolor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg>`,
   deleteIcon = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none"><path d="M21 5.98c-3.33-.33-6.68-.5-10.02-.5-1.98 0-3.96.1-5.94.3L3 5.98M8.5 4.97l.22-1.31C8.88 2.71 9 2 10.69 2h2.62c1.69 0 1.82.75 1.97 1.67l.22 1.3M18.85 9.14l-.65 10.07C18.09 20.78 18 22 15.21 22H8.79C6 22 5.91 20.78 5.8 19.21L5.15 9.14M10.33 16.5h3.33M9.5 12.5h5" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>`,
   todoArray = [],
@@ -15,6 +19,7 @@ const todoDisplay = () => {
         checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.className = "form-check-input selectTodo d-inline-flex";
+        checkbox.addEventListener("click", (e) => selectTodo(e));
 
         editBtn = document.createElement("button");
         editBtn.type = "button";
@@ -38,7 +43,7 @@ const todoDisplay = () => {
         secondCell.innerText = todoIndex + 1;
 
         thirdCell = tr.insertCell(2);
-        thirdCell.className = "align-middle";
+        thirdCell.className = "align-middle todoText";
         thirdCell.innerText = todoItem.todo;
 
         fourCell = tr.insertCell(3);
@@ -54,7 +59,7 @@ const todoDisplay = () => {
         tr.id = todoItem.id;
         todoTableBody.appendChild(tr);
       })
-    : console.log("todo is empty");
+    : (todoTableBody.innerHTML += `<tr><td colspan="5" class="text-center">No Todo Task</td></tr>`);
 };
 
 const todoValidation = () => {
@@ -80,16 +85,35 @@ todoForm.addEventListener("submit", (submitEvent) => {
   todoReset();
 });
 
+const selectTodo = (e) => {
+  let selectedArray = [];
+  let allEqual = (arr) => arr.every((val) => val === arr[0]);
+  let selectTodo = document.querySelectorAll(".selectTodo");
+  selectTodo.forEach((e) => {
+    selectedArray.push(e.checked);
+  });
+  const allChecked = allEqual(selectedArray);
+  selectedArray.forEach((e) => {
+    if (e !== false) {
+      todoSelectAll.checked = allChecked === true ? true : false;
+    }
+  });
+};
+
+todoSelectAll.addEventListener("click", (e) => {
+  let checkboxes = document.querySelectorAll(".selectTodo");
+  e.target.checked ? checkboxes.forEach((e) => (e.checked = true)) : checkboxes.forEach((e) => (e.checked = false));
+});
+
 const checkTodo = () => {
   todoArray = JSON.parse(localStorage.getItem("NewToDo")) || [];
   const checkAddedTodo = (data) => {
     return data.todo === todoInput.value;
-  }
+  };
   const inputToDo = todoArray.find(checkAddedTodo);
-  console.log(inputToDo === undefined ? "moklo" : "rehvado")
-  const inputToDoReturn = inputToDo === undefined ? false : true
+  const inputToDoReturn = inputToDo === undefined ? false : true;
   return inputToDoReturn;
-}
+};
 
 const editTodoMode = (e) => {
   todoArray = JSON.parse(localStorage.getItem("NewToDo")) || [];
@@ -128,6 +152,37 @@ const deleteTodo = (e) => {
   localStorage.setItem("NewToDo", JSON.stringify(todoArray));
   todoDisplay();
 };
+
+deleteAllTodo.addEventListener("click", () => {
+  todoArray.length > 0
+    ? confirm("Are you sure? want to delete all") &&
+      (localStorage.removeItem("NewToDo"), (todoArray = JSON.parse(localStorage.getItem("NewToDo")) || []), todoDisplay())
+    : alert("Bro, List is already empty. Look first");
+});
+
+selectedTodoDeleteBtn.addEventListener("click", () => {
+  const selectedTodo = document.querySelectorAll('.selectTodo:checked');
+  todoArray = JSON.parse(localStorage.getItem("NewToDo")) || [];
+  selectedTodo.forEach((item, index) => {
+    const row = item.closest("tr").id;
+    todoArray.forEach((todo, index) => {
+      todo.id == row && todoArray.splice(index, 1)
+    })
+  })
+  localStorage.setItem("NewToDo", JSON.stringify(todoArray));
+  todoDisplay();
+});
+
+SearchTodoData.addEventListener("keyup", (e) => {
+  const searchValue = e.target.value.toLowerCase();
+  let todoRow = todoTableBody.getElementsByTagName('tr');
+  for (todoRowIndex = 0; todoRowIndex < todoRow.length; todoRowIndex++) {
+    let todoData = todoRow[todoRowIndex].querySelector('.todoText');
+    let todoText = todoData.textContent;
+    console.log(todoText.indexOf(searchValue))
+    todoText.indexOf(searchValue) > -1 ? todoRow[todoRowIndex].style.display = "" : todoRow[todoRowIndex].style.display = "none"
+  }
+});
 
 (() => {
   todoArray = JSON.parse(localStorage.getItem("NewToDo")) || [];
